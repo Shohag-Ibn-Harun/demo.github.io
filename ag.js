87c51e85ca6cb161d3ebccdcb9757e80c3c700ec
Shohag Ibn Harun <shohagibnharun@gmail.com>
2022-09-03 22:13:45 +0600

    update

---
+++ ag.js
@@ -0,0 +1,84 @@
+
+function opnbtn() {
+	document.getElementById('pop1').style.height="100%"
+}
+function opnbtnar() {
+	document.getElementById('pop2').style.height="100%"
+}
+function opnbtnho() {
+	document.getElementById('pop3').style.height="100%"
+}
+function opnbtnjo() {
+	document.getElementById('pop4').style.height="100%"
+}
+function opnbtnam() {
+	document.getElementById('pop5').style.height="100%"
+}
+function opnbtnal() {
+	document.getElementById('pop6').style.height="100%"
+}
+function opnbtntu() {
+	document.getElementById('pop7').style.height="100%"
+}
+function opnbtnad() {
+	document.getElementById('pop8').style.height="100%"
+}
+function opnbtnhr() {
+	document.getElementById('pop9').style.height="100%"
+}
+function opnbtnmd() {
+	document.getElementById('pop10').style.height="100%"
+}
+
+function clsbtn1() {
+	document.getElementById('pop1').style.height="0"
+}
+function clsbtn2() {
+	document.getElementById('pop2').style.height="0"
+}
+function clsbtn3() {
+	document.getElementById('pop3').style.height="0"
+}
+function clsbtn4() {
+	document.getElementById('pop4').style.height="0"
+}
+function clsbtn5() {
+	document.getElementById('pop5').style.height="0"
+}
+function clsbtn6() {
+	document.getElementById('pop6').style.height="0"
+}
+function clsbtn7() {
+	document.getElementById('pop7').style.height="0"
+}
+function clsbtn8() {
+	document.getElementById('pop8').style.height="0"
+}
+function clsbtn9() {
+	document.getElementById('pop9').style.height="0"
+}
+function clsbtn10() {
+	document.getElementById('pop10').style.height="0"
+}
+
+function knon1() {
+    document.getElementById('odr1').style.width="100%"
+}
+function clsknon1() {
+    document.getElementById('odr1').style.width="0"
+}
+function knon2() {
+    document.getElementById('odr2').style.width="100%"
+}
+function clsknon2() {
+    document.getElementById('odr2').style.width="0"
+}
+function knon3() {
+    document.getElementById('odr3').style.width="100%"
+}
+function clsknon3() {
+    document.getElementById('odr3').style.width="0"
+}
+function higt() {
+	document.getElementById('pod').style.height="1500px"
+}
